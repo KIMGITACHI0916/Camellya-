c0233575ce1674ba938b54d370f6937d5dccf06e
KIMGITACHI0916 <148501410+KIMGITACHI0916@users.noreply.github.com>
2025-04-28 14:49:39 +0530

    Delete Bot directory

--- Bot/handlers/start.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-
-def register(bot: Client):
-    @bot.on_message(filters.command("start") & filters.private)
-    async def start_handler(client, message):
-        await message.reply_text("Hello! Your bot is alive and working.")
